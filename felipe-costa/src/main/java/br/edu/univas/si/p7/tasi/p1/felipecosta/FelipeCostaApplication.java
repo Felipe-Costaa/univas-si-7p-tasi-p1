76ffbcf6c00e5e70489b4dc3e7df5101b1d72a75
Felipe Costa <felipecostamoreira@gmail.com>
2023-07-03 23:30:12 -0300

    Primeiro Commit da prova

---
+++ felipe-costa/src/main/java/br/edu/univas/si/p7/tasi/p1/felipecosta/FelipeCostaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.univas.si.p7.tasi.p1.felipecosta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelipeCostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FelipeCostaApplication.class, args);
+	}
+
+}
---
+++ felipe-costa/src/test/java/br/edu/univas/si/p7/tasi/p1/felipecosta/FelipeCostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.univas.si.p7.tasi.p1.felipecosta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FelipeCostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
